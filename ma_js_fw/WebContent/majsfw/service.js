52712fbca3dc0660fb1d7ca513a5ac955c5734cd
chris-peng <penghaifeng@foxmail.com>
2019-03-23 11:49:21 +0800

    Update service.js

--- ma_js_fw/WebContent/majsfw/service.js
+++ ma_js_fw/WebContent/majsfw/service.js
@@ -3,45 +3,59 @@
  */
 Mafw.ns('service', {
 	defaultConfig: {
-		method: 'get',
 		type: 'get',
 		dataType: 'json'
 	},
 	_services: {
-		getUsers: {
-			url: 'http://www.baidu2.com',
-			name: 'u'
-		},
-		getProds: {
-			url: 'http://www.baidu.com',
-			name: 'p'
-		}
+		
 	},
-	s: function(servName, aJaxConfig){
-		if(!Mafw.service._services[servName]){
-			throw '没有定义名为' + servName + '的service！';
+	_options: function(serviceScope, servName){
+		if(!serviceScope[servName]){
+			throw '没有定义名为' + serviceScope + '.' + servName + '的service！';
 		}
-		$.ajax($.extend({}, Mafw.service.defaultConfig, Mafw.service._services[servName], aJaxConfig));
+		return $.extend({}, Mafw.service.defaultConfig, serviceScope._services[servName]);
 	},
-	options: function(servName){
-		if(!Mafw.service._services[servName]){
-			throw '没有定义名为' + servName + '的service！';
-		}
-		return Mafw.service._services[servName];
-	},
-	init: function(){
-		function getServiceFunc(servName){
+	init: function(scope){
+		var serviceScope = scope || Mafw.service;
+		function getServiceFunc(serviceScope, servName){
 			return function(aJaxConfig){
-				var options = Mafw.service.options(servName);
-				$.ajax($.extend({}, Mafw.service.defaultConfig, options, aJaxConfig));
+				var options = Mafw.service._options(serviceScope, servName);
+				var mergedOptions = $.extend({}, options, aJaxConfig);
+				if(mergedOptions.url && mergedOptions.pathParams){
+					for(var pName in mergedOptions.pathParams){
+						mergedOptions.url = mergedOptions.url.replace('{' + pName + '}', mergedOptions.pathParams[pName]);
+					}
+				}
+				if(mergedOptions.url && mergedOptions.appendParams){
+					if(mergedOptions.url.indexOf('?') > 0){
+						mergedOptions.url += "&";
+					}else{
+						mergedOptions.url += "?";
+					}
+					mergedOptions.url += Mafw.service.object2Params(mergedOptions.appendParams);
+				}
+				if(mergedOptions.loadingMask){
+					Mafw.util.loading.show();
+				}
+				$.ajax(mergedOptions);
 			};
 		}
-		for(var servName in Mafw.service._services){
-			Mafw.service[servName] = getServiceFunc(servName);
+		for(var servName in serviceScope._services){
+			serviceScope[servName] = getServiceFunc(serviceScope, servName);
+			serviceScope[servName].options = Mafw.service._options(serviceScope, servName);
 		}
+	},
+	object2Params: function(obj){
+		var params = '';
+		for(var key in obj){
+			params += key + '=' + obj[key] + '&';
+		}
+		return encodeURI(params.substring(0, params.length - 1));
+	},
+	register: function(scope, serviceObj){
+		serviceObj = $.extend({}, {_services: serviceObj});
+		Mafw.ns('service.' + scope, serviceObj);
+		Mafw.service.init(serviceObj);
 	}
 });
 Mafw.service.init();
-
-$("head").append('<link id="css-theme" rel="stylesheet" type="text/css" href="css/test.css">');
-$("head #css-theme").remove();
